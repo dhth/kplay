99c2ff6e3b1f41c15a74660f95d4e76617bd49c0
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-09-21 15:25:50 +0200

    include decode error in scan output

--- internal/scan/scan.go
+++ internal/scan/scan.go
@@ -259,7 +259,7 @@ func newMessageWriter(filePath string, decode bool) (*messageWriter, error) {
 	rw.csvWriter = csv.NewWriter(rw.writer)
 	headers := []string{"partition", "offset", "timestamp", "key", "tombstone"}
 	if decode {
-		headers = append(headers, "decode_success")
+		headers = append(headers, "decode_error")
 	}
 
 	err = rw.csvWriter.Write(headers)
@@ -290,11 +290,11 @@ func (rw *messageWriter) writeCSV(msg t.Message, decode bool) error {
 	}
 
 	if decode {
-		if msg.DecodeErr == nil {
-			row = append(row, "true")
-		} else {
-			row = append(row, "false")
+		var decodeErr string
+		if msg.DecodeErr != nil {
+			decodeErr = msg.DecodeErr.Error()
 		}
+		row = append(row, decodeErr)
 	}
 
 	return rw.csvWriter.Write(row)
