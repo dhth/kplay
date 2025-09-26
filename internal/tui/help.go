7147ae798c233fd47b7c46aa161195e426f7a156
Dhruv Thakur <13575379+dhth@users.noreply.github.com>
2025-09-26 10:33:46 +0200

    reorder help pane items

--- internal/tui/help.go
+++ internal/tui/help.go
@@ -29,12 +29,14 @@ kplay has 2 views:
 	helpHeaderStyle.Render("Message List/Details View"),
 	helpSectionStyle.Render(`
     <tab>/<shift-tab>              Switch focus between panes
-    j/<Down>                       Select next item/scroll details down
-    k/<Up>                         Select previous item/scroll details up
-    g                              Select first item/scroll details to top
-    G                              Select last item/scroll details to bottom
-    <ctrl+u>                       Scroll details half page up
+    j/<Down>                       Select next message/scroll details down
+    k/<Up>                         Select previous message/scroll details up
+    G                              Select last message/scroll details to bottom
+    g                              Select first message/scroll details to top
     <ctrl+d>                       Scroll details half page down
+    <ctrl+u>                       Scroll details half page up
+    ]                              Select next message
+    [                              Select previous message
     n                              Fetch the next message from the topic
     N                              Fetch the next 10 messages from the topic
     }                              Fetch the next 100 messages from the topic
@@ -46,7 +48,5 @@ kplay has 2 views:
                                        messages/<topic>/partition-<partition>/offset-<offset>.txt)
     P                              Persist current message to local filesystem
     y                              Copy message details to clipboard
-    [                              Move to previous item in list
-    ]                              Move to next item in list
 `),
 )
